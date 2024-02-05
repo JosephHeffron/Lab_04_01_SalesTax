9a30b5822bf39df944defce6099804f84eeb825b
JosephHeffron <heffroja@mail.uc.edu>
2024-02-05 00:25:55 -0500

    Initial commit

---
+++ src/SalesTaxCalculator.java
@@ -0,0 +1,15 @@
+    public class SalesTaxCalculator {
+        public static void main(String[] args) {
+            // Given price of a purchase
+            double purchasePrice = 100.00;
+
+            // Compute sales tax (5% of the purchase price)
+            double salesTax = purchasePrice * 0.05;
+
+            // Output statements
+            System.out.println("Price of the purchase: $" + purchasePrice);
+            System.out.println("Sales tax (5%): $" + salesTax);
+        }
+    }
+
+}
